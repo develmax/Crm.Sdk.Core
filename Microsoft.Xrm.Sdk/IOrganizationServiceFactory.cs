9f70d681028876b586c3f7d19cb304e0271f24c1
develmax <develmax@gmail.com>
2020-08-13 02:14:39 +0300

    add IOrganizationServiceFactory

---
+++ Microsoft.Xrm.Sdk/IOrganizationServiceFactory.cs
@@ -0,0 +1,19 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: Microsoft.Xrm.Sdk.IOrganizationServiceFactory
+// Assembly: Microsoft.Xrm.Sdk, Version=7.0.0.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35
+// MVID: 917F618E-83AC-4359-819B-E0D742B6B85B
+// Assembly location: D:\Projects.Hg\prod_15\Sources\Bcs.Crm\packages\Microsoft.CrmSdk.CoreAssemblies.7.0.0.1\lib\net45\Microsoft.Xrm.Sdk.dll
+
+using System;
+
+namespace Microsoft.Xrm.Sdk
+{
+  /// <summary>Represents a factory for creating <see cref="T:Microsoft.Xrm.Sdk.IOrganizationService"></see> instances.</summary>
+  public interface IOrganizationServiceFactory
+  {
+    /// <summary>Returns an <see cref="T:Microsoft.Xrm.Sdk.IOrganizationService"></see> instance for the organization that the specified user is a member of.</summary>
+    /// <returns>Type: <see cref="T:Microsoft.Xrm.Sdk.IOrganizationService"></see>An organization service factory object.</returns>
+    /// <param name="userId">Type: Returns_Nullable&lt;Returns_Guid&gt;. Specifies the system user that calls to the service are made for.  A null value indicates the SYSTEM user. When called in a plug-in, a Guid.Empty value indicates the same user as <see cref="T:Microsoft.Xrm.Sdk.IPluginExecutionContext"></see>. <see cref="P:Microsoft.Xrm.Sdk.IExecutionContext.UserId"></see>. When called in a custom workflow activity, a  Guid.Empty value indicates the same user as IWorkflowExecutionContext.<see cref="P:Microsoft.Xrm.Sdk.IExecutionContext.UserId"></see>. Any other value indicates a specific system user. </param>
+    IOrganizationService CreateOrganizationService(Guid? userId);
+  }
+}
